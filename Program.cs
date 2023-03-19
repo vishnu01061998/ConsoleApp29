452e08cfe8fa664c6228cd46c89d4d602f61a5d8
HP <HP@DESKTOP-4Q8V1JF>
2023-03-20 01:07:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter Any Number");
+           int num = Convert.ToInt32(Console.ReadLine());
+           for (int i = 1; i <= num; ++i)
+           {
+              if (num % i == 0)
+              {
+                 Console.WriteLine(i);
+              }
+           }
+        }
+        
+       
+    }
+    
+}
